e06e2fde5077c3772f0c2e3cb775438822c7c044
Chaoching Vang <chaoching.vang@gmail.com>
2022-01-25 11:35:41 -0600

    changed route url

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
         <CssBaseline />
         <Navbar />
         <Switch>
-            <Route exact path="/">
+            <Route exact path="/personal-website">
               <Home />
             </Route>
             {/* <Route exact path="/personal-website">
